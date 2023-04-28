18883006bec8045fc5ae5235dbe2fd9289139286
Dhruv Shah <115712795+dhruv0703@users.noreply.github.com>
2023-04-28 17:08:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My favt food</h1>
+    <ul>
+      <li> Bacon</li>
+      <li> Noodeles </li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
